4a4e7015a0bea6f27e7ddec3dcce2dbd0b90d9ef
ilbuonmarcio <marciozgaming@gmail.com>
2020-04-30 22:11:58 +0200

    Optimized run time and added skipping of completely finished folders

--- augmenter.py
+++ augmenter.py
@@ -11,12 +11,15 @@ if __name__ == "__main__":
 
         input_chapter_folder = path
         output_chapter_folder = 'Augmented/' + path.replace('Downloads/', '')
-        
-        files = [f[2] for f in os.walk(input_chapter_folder)][0]
+
+        if Path(output_chapter_folder).is_dir():
+            if len(os.listdir(input_chapter_folder)) == len(os.listdir(output_chapter_folder)):
+                print(f"Directory {output_chapter_folder} already augmented entirely, skipping...")
+                continue
 
         Path(output_chapter_folder).mkdir(exist_ok=True)
-        for f in files:
-            os.system(f".\waifu2x-ncnn-vulkan.exe -i {input_chapter_folder}/{f} -o {output_chapter_folder}/{f} -s 2")
+        print(f"Augmenting folder {input_chapter_folder} into {output_chapter_folder}...")
+        os.system(f".\waifu2x-ncnn-vulkan.exe -v -i {input_chapter_folder} -o {output_chapter_folder} -s 2")
 
         
 
