438a840a63b5a9ee2b51a6a77fea8c5d42c2a77d
ilbuonmarcio <marciozgaming@gmail.com>
2020-05-01 14:57:54 +0200

    Added proper handling for failed jobs and output of failed job folders

--- augmenter.py
+++ augmenter.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 import os
 from pprint import pprint
+import time
+from subprocess import Popen, PIPE
 
 if __name__ == "__main__":
     Path('Augmented').mkdir(exist_ok=True)
+
+    augmentation_error_paths = []
 
     for path, _, files in os.walk('Downloads/'):
         if len(files) == 0:
@@ -17,10 +21,37 @@ if __name__ == "__main__":
                 print(f"Directory {output_chapter_folder} already augmented entirely, skipping...")
                 continue
 
+        
         Path(output_chapter_folder).mkdir(exist_ok=True)
         print(f"Augmenting folder {input_chapter_folder} into {output_chapter_folder}...")
-        os.system(f".\waifu2x-ncnn-vulkan.exe -v -i {input_chapter_folder} -o {output_chapter_folder} -s 2")
+        start_time = time.time()
 
-        
+        process = Popen(
+            [
+                '.\waifu2x-ncnn-vulkan.exe',
+                '-v',
+                '-i',
+                input_chapter_folder,
+                '-o',
+                output_chapter_folder,
+                '-s',
+                '2'
+            ],
+            stdout=PIPE,
+            stderr=PIPE
+        )
+        stdout, stderr = process.communicate()
 
+        if ' failed' in str(stderr, encoding='utf-8'):
+            augmentation_error_paths.append(input_chapter_folder)
+            print(f"One or more files on input folder {input_chapter_folder} has some erroneous file in it, check it and rerun the application for augmenting those files")
+        else:
+            print(f"Folder {input_chapter_folder} augmented successfully in {time.time() - start_time}s\n")
 
+    if len(augmentation_error_paths) == 0:
+        print("There are no errors found in the input directories, all files are augmented correctly!")
+    else:
+        print("The following input paths has some errors in it, check it manually and rerun the application")
+        for path in augmentation_error_paths:
+            print(path)
+        print("Rerun the augmenter after you fixed all those paths")
