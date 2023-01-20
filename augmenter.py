3c777e37b450439ea512577cb62b3294dc1038f1
ilbuonmarcio <marciozgaming@gmail.com>
2023-01-20 01:26:46 +0100

    Switched to a more open source oriented upscaling, also improved performance by far!

--- augmenter.py
+++ augmenter.py
@@ -3,55 +3,48 @@ import os
 from pprint import pprint
 import time
 from subprocess import Popen, PIPE
+import cv2
+from cv2 import dnn_superres
+
+sr = dnn_superres.DnnSuperResImpl_create()
 
 if __name__ == "__main__":
     Path('Augmented').mkdir(exist_ok=True)
 
     augmentation_error_paths = []
 
-    for path, _, files in os.walk('Downloads/'):
-        if len(files) == 0:
-            continue
+    for path in next(os.walk('Downloads/'))[1]:
+        input_chapter_folder = 'Downloads/' + path
+        output_chapter_folder = 'Augmented/' + path
 
-        input_chapter_folder = path
-        output_chapter_folder = 'Augmented/' + path.replace('Downloads/', '')
+        print(f"Searching on {input_chapter_folder} ->")
 
         if Path(output_chapter_folder).is_dir():
             if len(os.listdir(input_chapter_folder)) == len(os.listdir(output_chapter_folder)):
                 print(f"Directory {output_chapter_folder} already augmented entirely, skipping...")
                 continue
-
         
         Path(output_chapter_folder).mkdir(exist_ok=True)
-        print(f"Augmenting folder {input_chapter_folder} into {output_chapter_folder}...")
+        print(f"Augmenting folder {input_chapter_folder} into {output_chapter_folder}...", end="")
         start_time = time.time()
 
-        process = Popen(
-            [
-                '.\waifu2x-ncnn-vulkan.exe',
-                '-v',
-                '-i',
-                input_chapter_folder,
-                '-o',
-                output_chapter_folder,
-                '-s',
-                '2'
-            ],
-            stdout=PIPE,
-            stderr=PIPE
-        )
-        stdout, stderr = process.communicate()
+        for filename in os.listdir(input_chapter_folder):
+            # print(f"Augmenting file {filename}")
+            print(".", end="", flush=True)
+            # Read image
+            image = cv2.imread(input_chapter_folder + '/' + filename)
 
-        if ' failed' in str(stderr, encoding='utf-8'):
-            augmentation_error_paths.append(input_chapter_folder)
-            print(f"One or more files on input folder {input_chapter_folder} has some erroneous file in it, check it and rerun the application for augmenting those files")
-        else:
-            print(f"Folder {input_chapter_folder} augmented successfully in {time.time() - start_time}s\n")
+            # Read the desired model
+            path = "FSRCNN_x4.pb"
+            sr.readModel(path)
 
-    if len(augmentation_error_paths) == 0:
-        print("There are no errors found in the input directories, all files are augmented correctly!")
-    else:
-        print("The following input paths has some errors in it, check it manually and rerun the application")
-        for path in augmentation_error_paths:
-            print(path)
-        print("Rerun the augmenter after you fixed all those paths")
+            # Set the desired model and scale to get correct pre- and post-processing
+            sr.setModel("fsrcnn", 4)
+
+            # Upscale the image
+            result = sr.upsample(image)
+
+            # Save the image
+            cv2.imwrite(output_chapter_folder + '/' + filename, result)
+
+        print(f" ({round(time.time() - start_time, 3)}s)\n")
